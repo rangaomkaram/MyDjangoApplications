ea54e240c208ae0134c7fd55c55ce26d860a9d16
rangaomkaram <omkaram.ranga@gmail.com>
2022-06-10 09:07:41 +0530

    view adding

--- projectweb/views.py
+++ projectweb/views.py
@@ -4,20 +4,22 @@ from django.http import HttpResponse
 
 projectslist = [
 {
- 'id': '1',
-    'Project Name' : 'e-commerce website',
-    'Descripition' : 'This is the e-ecommerece wesite and makerting place to sell and buy products in online'
-
+    'id': '1',
+    'Project_Name' : 'e-commerce website',
+    'Description' : 'This is the e-ecommerece wesite and makerting place to sell and buy products in online',
+    'TopRated' :True
 },
 {
     'id' : '2',
-    'Project Name' : 'My Profile ',
-    'Description' : 'A personal Website explain about the me and writing articles , blogs e.t.c'
+    'Project_Name' : 'My Profile ',
+    'Description' : 'A personal Website explain about the me and writing articles , blogs e.t.c',
+    'TopRated' : False
 },
 {
     'id' : '3',
-    'Project Name' : 'Social Web app/ Website',
-    'Description' : 'A website that done by social networking the people and connecting share ideas and views'
+    'Project_Name' : 'Social Web app/ Website',
+    'Description' : 'A website that done by social networking the people and connecting share ideas and views',
+    'TopRated'    : True
 }
 ]
     
@@ -25,10 +27,16 @@ projectslist = [
 
 
 def Projects(request):
-    return render(request,'projectweb/projectweb.html')
+    context = {'projects' :projectslist}
+    return render(request,'projectweb/projectweb.html',context)
 
 def Project(request,pk):
-    return render(request,'projectweb/singlepage.html')
+    projectObject = None
+
+    for i in projectslist:
+        if i['id'] == str(pk):
+            projectObject = i
+    return render(request,'projectweb/singlepage.html',{'project':projectObject})
 
 def englishpage(request):
     name = "Ranga"
